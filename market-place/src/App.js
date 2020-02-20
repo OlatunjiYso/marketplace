95ce114473eade3fab91f8a272a995fdda167dd6
OlatunjiYso <olatunji.yusuf@audaciousfutures.co>
2020-02-21 00:07:22 +0100

    create header component

--- market-place/src/App.js
+++ market-place/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import './App.css';
 
 
@@ -10,9 +11,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <BrowserRouter>
+      <Header />
+      <Switch>
       <Route exact path='/' component={Homepage}/>
       <Route exact path='/shop' component={ShopPage}/> 
+      </Switch>
       </BrowserRouter>
     </div>
   );
