158a0aee115b585fa3d6e368c3b71a64c467a504
OlatunjiYso <olatunji.yusuf@audaciousfutures.co>
2020-02-20 20:11:38 +0100

    create collection-preview and collection-item components

--- market-place/src/App.js
+++ market-place/src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Homepage from './pages/homepage/homepage.component'
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1 style={{marginLeft: '50px'}}> HATs PAGE  </h1>
-  </div>
-)
-
-const JacketsPage = () => (
-  <div>
-    <h1 style={{marginLeft: '50px'}}> Jackets PAGE  </h1>
-  </div>
-)
-
-const 
-SneakersPage = () => (
-  <div>
-    <h1 style={{marginLeft: '50px'}}> Sneakers PAGE  </h1>
-  </div>
-)
 
 
 function App() {
@@ -29,9 +12,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Route exact path='/' component={Homepage}/>
-      <Route exact path='/hats' component={HatsPage}/>
-      <Route exact path='/jackets' component={JacketsPage}/>
-      <Route exact path='/sneakers' component={SneakersPage}/>
+      <Route exact path='/shop' component={ShopPage}/> 
       </BrowserRouter>
     </div>
   );
